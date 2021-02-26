614c694ca9d1beab9b6f851612f7b53172e11a7e
Prakash-PKG <prakash.garanaik18@gmail.com>
2021-02-27 00:58:53 +0530

    intial data

--- src/main/java/com/app/pk/SpringBootAngularSwaggerCurdAppApplication.java
+++ src/main/java/com/app/pk/SpringBootAngularSwaggerCurdAppApplication.java
@@ -8,6 +8,7 @@ public class SpringBootAngularSwaggerCurdAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularSwaggerCurdAppApplication.class, args);
+int a=20;
 	}
 
 }
